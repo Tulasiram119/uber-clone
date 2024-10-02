a16361dcf6177600b13d525b4a831121619b026a
tulasiram119 <tulasiramkancharla119.com>
2024-10-02 13:52:30 +0530

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,82 @@
+import CustomButton from "@/components/CustomButton";
+import InputFeild from "@/components/InputFeild";
+import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { useSignIn } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const router = useRouter();
+  const onSignInPress = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.push(`/(root)/(tabs)/home`);
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      Alert.alert("Error", err.errors[0].longMessage);
+    }
+  }, [isLoaded, form.email, form.password]);
+  return (
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputFeild
+            label="Email"
+            placeholder="Enter your email address"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputFeild
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry={true}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+          <CustomButton
+            title="Sign in"
+            className="mt-6"
+            onPress={onSignInPress}
+          />
+          <OAuth />
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-7"
+          >
+            <Text>Don't an account? </Text>
+            <Text className="text-primary-500">Sign up</Text>
+          </Link>
+        </View>
+        {/* Verification model */}
+      </View>
+    </ScrollView>
+  );
+};
+export default SignIn;
---
+++ components/RideLayout.tsx
@@ -0,0 +1,53 @@
+import { icons } from "@/constants";
+import { router } from "expo-router";
+import { ReactNode, useRef } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Map from "./Map";
+import BottomSheet from "@gorhom/bottom-sheet/lib/typescript/components/bottomSheet/BottomSheet";
+import { BottomSheetView } from "@gorhom/bottom-sheet";
+
+export default function RideLayout({
+  title,
+  children,
+  snapPoints,
+}: {
+  title: string;
+  children: ReactNode;
+  snapPoints?: string[];
+}) {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1 bg-white">
+        <View className="flex flex-col h-screen bg-blue-500">
+          <View className="flex flex-row absolute z-10 top-16 items-center justify-start px-5">
+            <TouchableOpacity onPress={() => router.back()}>
+              <View className="w-10 h-10 bg-white rounded-full items-center justify-center">
+                <Image
+                  source={icons.backArrow}
+                  resizeMode="contain"
+                  className="w-6 h-6"
+                />
+              </View>
+            </TouchableOpacity>
+            <Text className="text-xl font-JakartaSemiBold ml-5 ">
+              {title || "Go Back"}
+            </Text>
+          </View>
+          <Map />
+        </View>
+        <BottomSheet
+          keyboardBehavior="extend"
+          ref={bottomSheetRef}
+          snapPoints={snapPoints || ["40%", "85%"]}
+          index={0}
+        >
+          <BottomSheetView style={{ flex: 1, padding: 20 }}>
+            {children}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
+  );
+}
